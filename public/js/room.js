45044720b86341eb7602e8500ca79466fe83fda4
narrrrrrt <222976023+narrrrrrt@users.noreply.github.com>
2025-09-24 21:51:53 +0900

    1.1.1

--- public/js/room.js
+++ public/js/room.js
@@ -174,7 +174,7 @@ function renderStatus(status) {
           }
 
           // --- パスチェック ---
-          if (movesByColor[msg.data.status].length === 0) {
+          if (movesByColor[myRole].length === 0) {
             ws.send(JSON.stringify({
               event: "move",
               token: myToken,
